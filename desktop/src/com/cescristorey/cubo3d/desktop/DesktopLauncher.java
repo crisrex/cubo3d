8d39a0f09d509529abb99c759f1ef1381dc68b50
Luis Serrano <luis@cescristorey.com>
2019-02-16 09:54:08 +0100

    Commit inicial

---
+++ desktop/src/com/cescristorey/cubo3d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cescristorey.cubo3d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cescristorey.cubo3d.Cubo3D;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Cubo3D(), config);
+	}
+}
